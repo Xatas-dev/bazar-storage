ebafcbdbf9b0f95bc80e159db36721b1e0cc2d79
elSasen <seny-pnz2003@mail.ru>
2026-02-15 21:36:14 +0300

    init

---
+++ src/main/java/org/bazar/bazarstorage/BazarStorageApplication.java
@@ -0,0 +1,13 @@
+package org.bazar.bazarstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazarStorageApplication.class, args);
+    }
+
+}
