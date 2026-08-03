2cae715f3a22a6dfa8bd441148df6c28563c3546
Arseny Revunov <seny-pnz2003@mail.ru>
2026-08-03 23:24:47 +0300

    Добавлен столбец jsonb с ошибками узла вместо таблицы

--- src/main/resources/db/changelog/v1.0.2/001-create-storage-node-error-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE storage_node_error (
-    id BIGSERIAL PRIMARY KEY,
-    node_id BIGINT NOT NULL REFERENCES storage_node,
-    error_code VARCHAR NOT NULL,
-    description VARCHAR,
-    created_at timestamp with time zone not null default now(),
-    updated_at timestamp with time zone not null default now()
-);
